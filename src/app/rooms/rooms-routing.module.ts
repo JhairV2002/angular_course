6254c6d143db91b942f8d938c585f9156a9e704b
JhairV2002 <jag.vasquez@yavirac.edu.ec>
2022-12-16 20:25:27 -0500

    adds nested routes

--- src/app/rooms/rooms-routing.module.ts
+++ src/app/rooms/rooms-routing.module.ts
@@ -8,14 +8,16 @@ const routes: Routes = [
   {
     path: 'rooms',
     component: RoomsComponent,
-  },
-  {
-    path: 'rooms/add',
-    component: RoomsAddComponent,
-  },
-  {
-    path: 'rooms/:id',
-    component: RoomsBookingComponent,
+    children: [
+      {
+        path: 'add',
+        component: RoomsAddComponent,
+      },
+      {
+        path: ':id',
+        component: RoomsBookingComponent,
+      },
+    ],
   },
 ];
 
